8488cbb0e2755ef1c633d09fc72968e3ce74e09c
Mustafa Osman Tekin <mustafateknn@gmail.com>
2022-12-25 16:04:08 +0300

    implement typescript

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
