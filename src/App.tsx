44083517c5199048e7aad1f4753ad2165293d815
Mustafa Osman Tekin <mustafateknn@gmail.com>
2022-12-26 00:47:21 +0300

    add link for employee details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Employee from "./pages/Employee";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Employee />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/EmployeeCard.tsx
+++ src/components/EmployeeCard.tsx
@@ -1,4 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link, useNavigate } from 'react-router-dom';
 import { faEnvelope, faLocationPin, faBuilding, faGlobe } from '@fortawesome/free-solid-svg-icons'
 import { useAppDispatch } from '../store';
 import { vote } from '../store/features/employeeSlice';
@@ -7,16 +8,21 @@ import { User, IEmployeeCardProps } from '../types';
 export default function EmployeeCard(props: IEmployeeCardProps) {
     const { user } = props;
     const dispatch = useAppDispatch();
+    const navigate = useNavigate();
 
     const onClickVote = (user: User) => {
         dispatch(vote(user));
     };
 
+    const onClickImage = (id: string) => {
+        navigate(`/${id}`)
+    }
+
     return (
         <div className="card">
-            <img src={user.albums.data[0].photos.data[0].thumbnailUrl} alt="user-img" />
+            <img src={user.albums.data[0].photos.data[0].thumbnailUrl} alt="user-img" className='card-img' onClick={() => onClickImage(user.id)} />
             <div className="card-body">
-                <h3>{user.name}</h3>
+                <h3><Link to={`${user.id}`}>{user.name}</Link></h3>
                 <div className="card-details">
                     <p><FontAwesomeIcon icon={faEnvelope} width={16} /><span>{user.email}</span></p>
                     <p><FontAwesomeIcon icon={faBuilding} width={16} /><span>{user.company.name}</span></p>
