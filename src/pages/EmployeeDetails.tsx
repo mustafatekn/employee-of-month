671edf44e29ab5c54714346153d9e9bda8712181
Mustafa Osman Tekin <mustafateknn@gmail.com>
2022-12-26 02:19:48 +0300

    improve employee details header

--- src/pages/EmployeeDetails.tsx
+++ src/pages/EmployeeDetails.tsx
@@ -12,8 +12,30 @@ export default function EmployeeDetails() {
 
     return (
         <div className="container">
-            <header className='user-details-header'>
-                <img src={data.user.albums.data[0].photos.data[0].thumbnailUrl} className="profile-image" />
+            <header className='employee-details'>
+                <div className='employee-details-header' />
+                <div className='profile-image-wrapper'>
+                    <img src={data.user.albums.data[0].photos.data[0].thumbnailUrl} className="profile-image" />
+                </div>
+                <div className='employee-information-wrapper'>
+                    <div className='employee-information'>
+                        <h2>{data.user.name}</h2>
+                        <p>@{data.user.username.toLowerCase()}</p>
+                        <p>{data.user.company.name}</p>
+                    </div>
+
+                    <div className='communication'>
+                        <p>{data.user.email.toLowerCase()}</p>
+                        <p>{data.user.website}</p>
+                    </div>
+
+                    <div className='address'>
+                        <p>City: {data.user.address.city}</p>
+                        <p>Street: {data.user.address.street}</p>
+                        <p>Suite: {data.user.address.suite}</p>
+                    </div>
+                </div>
+
             </header>
         </div>
     )
