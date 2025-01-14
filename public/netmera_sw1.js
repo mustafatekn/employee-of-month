a0fd1a301a01753d4110ca43028cf3fa0e5e6049
Mustafa Osman Tekin <57020656+mustafatekn@users.noreply.github.com>
2025-01-14 12:06:39 +0300

    Revert "Update index.html"
    This reverts commit 59b454b7525574067753c686c88c44009aca6cf8.

--- public/netmera_sw1.js
+++ public/netmera_sw1.js
@@ -1,1 +1,35 @@
-var netmera;importScripts("https://ntm.netmera-web.com/wsdk2/netmera_sw.js?v=4.3.1"),netmera={};
+
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+var netmera;
+/******/ (() => { // webpackBootstrap
+	/******/ 	var __webpack_modules__ = ({
+
+		/***/ "./service_worker/netmera_sw1.js":
+		/*!***************************************!*\
+			!*** ./service_worker/netmera_sw1.js ***!
+			\***************************************/
+		/***/ (() => {
+
+			eval("importScripts(\"https://cloud-uat.netmera-web.com\" + '/wsdk2/netmera_sw.js?v=' + \"4.3.1\");\n\n//# sourceURL=webpack://netmera/./service_worker/netmera_sw1.js?");
+
+			/***/ })
+
+		/******/ 	});
+	/************************************************************************/
+	/******/
+	/******/ 	// startup
+	/******/ 	// Load entry module and return exports
+	/******/ 	// This entry module can't be inlined because the eval devtool is used.
+	/******/ 	var __webpack_exports__ = {};
+	/******/ 	__webpack_modules__["./service_worker/netmera_sw1.js"]();
+	/******/ 	netmera = __webpack_exports__;
+	/******/
+	/******/ })()
+;
